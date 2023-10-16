34ba51adf89515d8e7148e526dbaa91ad14c7a4d
yfellipe <yagofellipe09@gmail.com>
2023-10-16 09:22:46 -0300

    c09

---
+++ projects/c09/ex00/libft_creator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gcc -Wall -Werror -Wextra -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
+
+ar rvs libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
+
+find . -name "*.o" -type f -delete
+
