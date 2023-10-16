e475b98e22c61fe2504d4fe7c17fc4ffa4832cb6
yfellipe <yagofellipe09@gmail.com>
2023-10-16 15:58:52 -0300

    strlcpy

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -8,5 +8,26 @@ void *memmove(void *dest, const void *src, size_t n)
 	
 	d = (char *)dest;
 	s = (char *)src;
-	if 
+	if(d == s)
+		return (dest);
+	
+	if (d < s || d >= s + n)
+	{
+		i = 0;
+		while(i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else
+	{
+		i = n;
+		while (i > 0)
+		{
+			d[i-1] = s[i-1];
+			i--;
+		}
+	}
+	return (dest);
 }
