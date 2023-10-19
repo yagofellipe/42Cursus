48846f290b3df1aee5b69d9b59f47f5ee3e833f8
yagofellipe <yagofellipe09@gmail.com>
2023-10-19 14:40:47 -0300

    split1

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -1,42 +1,86 @@
-#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int	count_word(const char *s, char c)
-{
-	int	i;
-	int	j;
-	
-	i = 0;
-	j = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			j++;
-		i++;
-	}
-	return (j);
-} 
+static int countWords(const char *s, char c) {
+    int count = 0;
+    int in_word = 0;
 
+    while (*s) {
+        if (*s == c) {
+            in_word = 0;
+        } else if (in_word == 0) {
+            in_word = 1;
+            count++;
+        }
+        s++;
+    }
+
+    return count;
+}
+
+static char *strndup(const char *str, size_t n) {
+    char *dup = (char *)malloc(n + 1);
+    if (dup != NULL) {
+        strncpy(dup, str, n);
+        dup[n] = '\0';
+    }
+    return dup;
+}
 
 char **ft_split(char const *s, char c)
 {
 	int	i;
 	int	j;
-	int	numword;
-	char	**array;
+	int	num_words;
+	int	word_len;
+	char	**ptr;
 	
-	if (s == NULL)
-		return (NULL);
-	numword = count_word(s, c);
-	array = (char **)ft_calloc(sizeof(char *), (numword + 1);
-	if (!array)
-		return (NULL);
-	
-	i = 0;
-	j = 0;
-	while (s[i] != '\0' && j < n_words)
-	{
-		while (s[i] == c && s[i])
-			i++;
-	
-	}
+	if (!s)
+        return NULL;
+    num_words = countWords(s, c);
+    **ptr = (char **)malloc((num_words + 1) * sizeof(char *));
+    if (ptr == NULL)
+        return NULL;
+    i = 0;
+    j = 0;    
+    while (s[i] && j < num_words) {
+        while (s[i] == c && s[i])
+            i++;
+        if (s[i] != c) {
+            word_len = 0;
+            while (s[i + word_len] && s[i + word_len] != c)
+                word_len++;
+            ptr[j] = strndup(&s[i], word_len);
+            if (!ptr[j])
+                return NULL;            
+            j++;
+            i += word_len;
+        }
+    }
+    ptr[j] = NULL; // Adicione um ponteiro NULL para indicar o final do array de strings.
+    
+    return (ptr);
+}
+
+int main() {
+    const char *input = "Esta é uma string que você deseja dividir.";
+    char delimiter = ' ';
+
+    char **tokens = ft_split(input, delimiter);
+
+    if (tokens) {
+        int i = 0;
+        while (tokens[i]) {
+            printf("Token %d: %s\n", i, tokens[i]);
+            free(tokens[i]);
+            i++;
+        }
+
+        free(tokens);
+    } else {
+        perror("Erro na função ft_split");
+    }
+
+    return 0;
 }
