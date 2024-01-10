c6035a080cba6148e00b027aa501f0c14e2f0603
yfellipe <yagofellipe09@gmail.com>
2024-01-10 16:42:54 -0300

    client

--- minitalk/client.c
+++ minitalk/client.c
@@ -7,38 +7,38 @@ void	send_signal(int pid, char signal)
 	i = 7;
 	while (i >= 0)
 	{
-		if (signal & (1 << i)
+		if (signal & (1 << i))
 		{
 			kill(pid, SIGUSR1);
 		}
 		else
 			kill(pid, SIGUSR2);
-		usleep(100);
+		usleep(200);
 		i--;
 	}	
-	return (0);
 }
 
-int	main(int argc, char **argv)
+int	main(int ac, char **av)
 {
-	int	pid;
 	int	i;
-	
-	pid = ft_atoi(argv[1]);
-	if (argc =! 3)
+	int	pid;
+
+	i = 0;
+	if (ac != 3)
 	{
-		ft_printf("Verfique o número de argumentos\n");
-		return (0);		
-	}
-	if (kill(pid, 0) == -1)
-	{
-		ft_printf("pid errado");
+		ft_printf("Check the syntax: ./client server-pid text-to-send\n");
 		return (0);
 	}
-	i = 0;
-	while (argv[1][i] =! '\0')
+	if (kill(ft_atoi(av[1]), 0) == -1)
 	{
-		send_signal(pid, argv[1][i]);
+		ft_printf("Wrong pid, please check and try again!");
+		return (0);
+	}
+	pid = ft_atoi(av[1]);
+	i = 0;
+	while (av[2][i])
+	{
+		send_signal(pid, av[2][i]);
 		i++;
 	}
 	return (0);
